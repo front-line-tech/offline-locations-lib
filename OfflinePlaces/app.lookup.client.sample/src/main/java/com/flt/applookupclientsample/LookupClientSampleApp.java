713c5d857fa0e8d7578fe853934a740c8a47a8c8
Lewis Westbury <lewis@front-line-tech.com>
2018-08-22 01:51:43 +0100

    fix clashing versions of architecture components in build

---
+++ OfflinePlaces/app.lookup.client.sample/src/main/java/com/flt/applookupclientsample/LookupClientSampleApp.java
@@ -0,0 +1,9 @@
+package com.flt.applookupclientsample;
+
+import android.app.Application;
+
+public class LookupClientSampleApp extends Application {
+
+  public static String[] permissions_required = new String[] { };
+
+}
