17f4d361c64283896cd4397f91ef664197d0e5a4
Lewis Westbury <lewis@front-line-tech.com>
2018-09-08 21:26:52 +0100

    remove vestigial permissions from licensing library (add this and the downloader another time)

--- OfflinePlaces/app.looukp.provider/src/main/java/com/flt/applooukpprovider/LookupProviderApp.java
+++ OfflinePlaces/app.looukp.provider/src/main/java/com/flt/applooukpprovider/LookupProviderApp.java
@@ -8,7 +8,13 @@ import com.flt.liblookupprovider.LibLookup;
 public class LookupProviderApp extends Application {
 
   public static String[] permissions_required = new String[] {
-      Manifest.permission.READ_EXTERNAL_STORAGE
+      Manifest.permission.READ_EXTERNAL_STORAGE,
+//      Manifest.permission.INTERNET,
+//      Manifest.permission.WAKE_LOCK,
+//      Manifest.permission.ACCESS_NETWORK_STATE,
+//      Manifest.permission.ACCESS_WIFI_STATE,
+//      Manifest.permission.WRITE_EXTERNAL_STORAGE,
+//      "com.android.vending.CHECK_LICENSE",
   };
 
 }
